5ed12a441a6deac22ea849b4962a02144ef1ca6e
Your Name <nataliafariassantos48@gmail.com>
2024-12-14 02:04:52 -0300

    Style: Add logo

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-blue-700 p-6">
+    <header className="bg-gradient-to-r from-blue-900 to-blue-500 p-6">
       <div className="container mx-auto flex justify-center items-center">
         <nav className="space-x-4 text-white">
           <a href="#home" className="hover:text-red-700">Home</a>
