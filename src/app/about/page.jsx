8d0ef2b8e26028039d628380099120b2a4258ef4
Your Name <nataliafariassantos48@gmail.com>
2025-01-21 02:34:27 -0300

    REFACTORING: Code refactoring

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,4 +1,6 @@
+'use client';
 import * as React from 'react';
+import Image from 'next/image';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -10,7 +12,43 @@ import CardActions from '@mui/material/CardActions';
 export default function About() {
   return (
     <>
-      <div className="bg-blue-900 mx-auto px-4 py-10 "></div>{/*  //Cirrige temporáriamente a visisbilidade do header */}
+     <div className="relative w-full h-screen">
+          <Image
+            src="/images/fundoMenuPrincipal.jpg"
+            alt="fundo"
+            layout="fill"
+            objectFit="cover" // Ajuste para cobrir toda a área
+            className="w-full h-full"
+          />
+          <div className="absolute bottom-0 z-20 w-full flex justify-center"> {/* Ajuste para posicionar a imagem no final */}
+            <Image
+              src="/images/AboutImages/AtletaSFundo2.png"
+              alt="imagem sem fundo do banner"
+              layout="intrinsic"
+              width={1000} // Aumentando a largura para melhorar a qualidade
+              height={800} // Aumentando a altura para melhorar a qualidade
+              className="object-contain"
+              unoptimized={true}
+            />
+          </div>
+          <div className="relative z-30 flex flex-col items-center justify-center h-full text-white p-6">
+            <div className="absolute top-0 left-0 m-4">
+              <Image
+                src="/images/LogoRecortada.png"
+                alt="Logo"
+                width={100} // Largura ajustada para mobile
+                height={50} // Altura ajustada para mobile
+                className="object-contain md:w-80 md:h-64 mt-3" // Ajuste responsivo
+              />
+            </div>
+            <div className="text-center md:hidden w-full p-6 mt-[-160px]"> {/* Ajuste da margem superior */}
+              <h1 className="text-3xl font-bold text-white">Sobre</h1>
+              <hr className="border-t-6 border-gray-300 w-3/4 mx-auto my-4" /> {/* Linha suave com comprimento aumentado */}
+              <p className="mt-2 text-lg text-red-500">Conheça o nosso projeto social.</p>
+            </div>
+          </div>
+        </div>
+      
       <div className="container mx-auto px-4 py-8 mt-20">
         <h1 className="text-3xl font-bold mb-6 text-center">Sobre o Projeto Social de Boxe</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
