d182e5d9964529608f7517022234e2e556206947
Your Name <nataliafariassantos48@gmail.com>
2025-01-22 01:31:25 -0300

    Changes to the footer component

--- src/app/components/Layout/Footer.jsx
+++ src/app/components/Layout/Footer.jsx
@@ -1,26 +1,43 @@
 import React from 'react';
-import { FaInstagram, FaLaptop } from 'react-icons/fa'; //íncons
+import InstagramIcon from '@mui/icons-material/Instagram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import { FaInstagram, FaLaptop } from 'react-icons/fa'; // ícones
 
 const Footer = () => {
   return (
-    <footer className="bg-blue-800 text-white p-4 mt-8">
-      <div className="container mx-auto text-center">
-        <p>&copy; 2025 Nocaute clube boxe. Todos os direitos reservados.</p>
-       
-        <div className="space-x-4 mt-2">
-          <p>
-            <a href="https://www.instagram.com/nocaute_clube_se/?locale=gb&hl=am-et" target="_blank" rel="noopener noreferrer" className="hover:text-red-800 flex items-center justify-center">
-              <FaInstagram className="mr-2" /> Instagram
+    <>
+      <footer className="bg-gray-800 text-white mt-8 ">
+        <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 pt-4">
+          <h2 className="text-xl font-bold mb-2">Endereço</h2>
+          <p className="mb-2">Endereço: Rua Exemplo, 123, Bairro, Cidade, Estado</p>
+          <h2 className="text-xl font-bold mb-2">Entre em contato</h2>
+          <p className="mb-2">Telefone: (11) 1234-5678</p>
+          <div className="flex space-x-4">
+            <a href="https://wa.me/1234567890" target="_blank" rel="noopener noreferrer" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded inline-flex items-center">
+              <WhatsAppIcon className="mr-2" />
+              <span>WhatsApp</span>
             </a>
-          </p>
-          <p>
-            <a href="https://github.com/nataliasantosfarias" target="_blank" rel="noopener noreferrer" className="hover:text-red-800 flex items-center justify-center">
-              <FaLaptop className="mr-2" /> Desenvolvido por Natália Farias Santos
+            <a
+              href="https://www.instagram.com/nocaute_clube_se/?locale=gb&hl=am-et" target="_blank" rel="noopener noreferrer" className="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded inline-flex items-center">
+              <InstagramIcon className="mr-2" />
+              <span>Instagram</span>
             </a>
-          </p>
+          </div>
         </div>
-      </div>
-    </footer>
+        <div className="bg-red-500 text-white p-4 mt-12">
+          <div className="container mx-auto text-center">
+            <p>&copy; 2025 Nocaute clube boxe. Todos os direitos reservados.</p>
+            <div className="space-x-4 mt-2">
+              <p>
+                <a href="https://github.com/nataliasantosfarias" target="_blank" rel="noopener noreferrer" className="hover:text-blue-800 flex items-center justify-center">
+                  <FaLaptop className="mr-2" /> Desenvolvido por Natália Farias Santos
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 };
 
