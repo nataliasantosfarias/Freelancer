9e637d1c09b898fce2ffcb576bb4cbb9462ec33c
Your Name <nataliafariassantos48@gmail.com>
2025-02-01 12:41:24 -0300

    fix: remove buttons links that direct to social networks

--- src/app/components/Layout/Header.jsx
+++ src/app/components/Layout/Header.jsx
@@ -3,10 +3,6 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { menuLinks } from "@/app/constants";
-import InstagramIcon from '@mui/icons-material/Instagram';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-
-
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -45,24 +41,7 @@ const Header = () => {
             <Link key={link.href} href={link.href} className="hover:text-red-700"><span className="font-bold">{link.name}</span></Link>
           ))}
         </div> {/* mapeando os links */}
-        <div className="hidden md:flex space-x-4"> {/* Adicionando os botões em telas maiores */}
-            <a
-              href="https://wa.me/1234567890"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded inline-flex items-center"
-            >
-              <WhatsAppIcon className="mr-2" />
-            </a>
-            <a
-              href="https://www.instagram.com/nocaute_clube_se/?locale=gb&hl=am-et"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-pink-700 hover:bg-pink-500 text-white font-bold py-2 px-4 rounded inline-flex items-center"
-            >
-              <InstagramIcon className="mr-2" />
-            </a>
-          </div>
+        
         <div className="md:hidden text-white ml-auto cursor:pointer" onClick={toggleSidebar}>
           {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
         </div>
