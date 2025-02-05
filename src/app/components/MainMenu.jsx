7311ad29f3a17650004838cb755750aedab03545
Your Name <nataliafariassantos48@gmail.com>
2025-02-04 23:47:38 -0300

    REMOVE: Text component MAinMneu

--- src/app/components/MainMenu.jsx
+++ src/app/components/MainMenu.jsx
@@ -24,19 +24,23 @@ export default function MainMenu({ bannerImage, backgroundImage, backgroundColor
         )
       }
       <div className="absolute bottom-0 z-20 w-full flex justify-center"> {/* Ajuste para posicionar a imagem no final */}
-        <Image
-          // A prop `bannerImage` é usada aqui para definir a imagem do banner.
-          // Se `bannerImage` for fornecida, ela será usada como a fonte da imagem.
-          // Caso contrário, a imagem padrão "/images/victoriaSFundoBanner.png" será usada.
-          src={bannerImage || "/images/victoriaSFundoBanner.png"}
-          alt="imagem sem fundo do banner"
-          layout="intrinsic"
-          width={600} // Aumentando a largura para melhorar a qualidade
-          height={400} // Aumentando a altura para melhorar a qualidade
-          className={`object-contain ${styles.animateImage}`} // Aplicando a classe de animação
-          
-          unoptimized={true}
+        {
+          bannerImage && (
+          <Image
+            // A prop `bannerImage` é usada aqui para definir a imagem do banner.
+            // Se `bannerImage` for fornecida, ela será usada como a fonte da imagem.
+            // Caso contrário, a imagem padrão "/images/victoriaSFundoBanner.png" será usada.
+            src={bannerImage}
+            alt="imagem sem fundo do banner"
+            layout="intrinsic"
+            width={600} // Aumentando a largura para melhorar a qualidade
+            height={400} // Aumentando a altura para melhorar a qualidade
+            className={`object-contain ${styles.animateImage}`} // Aplicando a classe de animação
+            
+            unoptimized={true}
         />
+          )
+        }
       </div>
       <div className="relative z-30 flex flex-col items-center justify-center h-full text-white p-6">
         <div className="absolute top-0 left-0 m-4">
@@ -52,12 +56,6 @@ export default function MainMenu({ bannerImage, backgroundImage, backgroundColor
         <div className='absolute top-20 right-14 hidden md:block'>
           <ButtonEnroll />
         </div>
-
-        <div className="text-center md:hidden w-full p-6 mt-[-160px]"> {/* Ajuste da margem superior */}
-          <h1 className="text-2xl font-bold text-white">Nocaute Clube Boxe</h1>
-          <hr className="border-t-6 border-gray-300 w-3/4 mx-auto my-4" /> {/* Linha suave com comprimento aumentado */}
-          <h2 className="mt-2 text-xl font-bold text-red-500">{message}</h2> 
-        </div> 
       </div>
     </div>
   );
